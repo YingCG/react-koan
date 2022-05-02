0a8aa091c7d9b81b406b247ab8fc6f2a02da99a2
YingCG <ying@DESKTOP-VQDFUEH.localdomain>
2022-05-03 09:51:11 +1200

    change to functional component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-constructor(){
+function App() {
+  const [content, setContent] = useState({
+    hello: " Get Your Coffee Ready! ",
+    task1: 'Above is a hardcoded... To dynamic render information, we can use an OnClick event!',
+    task2: ''
+  })
 
-  // function showNext(){
-  //   this.setState('Replace the button to a Button Component')
-  // }
-super()
-
-this.state = {
-name: 'Dream Walker',
-todayGoal: "Get Your Coffee Ready! ",
-task1: 'This is a hardcoded... To dynamic render information, we can use an OnClick event! ',
-task2: 'Replace the button to a Button Component '
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
+        <p> {content.hello}</p>
+        <h3 className='problems'>{content.task1}</h3>
+        <button onClick={() => {
+            setContent( (info) => {
+                return { 
+                  ...info, 
+                  hello: 'Today we are making React Sandwich', 
+                  task1: 'When you click, above line shall change to --> Today we are making React Sandwich',
+                  task2: 'You can now replace the button with Button component'
+                }
+              })
+            }
+          }
+        >Let's Do It!</button>
+      </header>
+    </div>
+  );
 }
-}
 
-
-
-render(){
-return (
-<div className="App">
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p> Kiora, {this.state.name}</p>
-    <p style={{color: '#28FFBF'}}> {this.state.todayGoal}</p>
-    <h3 className='problems'>{this.state.task1}</h3>
-    <button 
-    // onClick={ ()=> {this.setState(
-    //           () => { return {name: 'Coffee Head! ', todayGoal: 'Today we are making React Sandwich'}},
-    //           () => console.log('Next!'))}
-    //         }
-    >Let's Do It!</button>
-  </header>
-</div>
-);
-}
-}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <h1>Kiora, today we are making react sandwich</h1>
+    <h1 className='problems'>To start, you can remove all replace h1 to App component here</h1>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
