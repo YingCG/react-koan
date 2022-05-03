b2fb36cc34ff9d71294652ec8d373778ba194249
YingCG <ying@DESKTOP-VQDFUEH.localdomain>
2022-05-03 20:24:38 +1200

    not sure

--- src/App.js
+++ src/App.js
@@ -3,31 +3,34 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [content, setContent] = useState({
-    hello: " Get Your Coffee Ready! ",
-    task1: 'Above is a hardcoded... To dynamic render information, we can use an OnClick event!',
-    task2: ''
-  })
+  const [task, setTask] = useState('Above is a hardcoded... To dynamic render information, we can use onClick event!')
+  const [hello, setHello] = useState('Get Your Coffee Ready!')
+  const [showTips, setShowTips] = useState('')
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
-        <p> {content.hello}</p>
-        <h3 className='problems'>{content.task1}</h3>
-        <button onClick={() => {
-            setContent( (info) => {
-                return { 
-                  ...info, 
-                  hello: 'Today we are making React Sandwich', 
-                  task1: 'When you click, above line shall change to --> Today we are making React Sandwich',
-                  task2: 'You can now replace the button with Button component'
-                }
-              })
+        <p> {hello}</p>
+
+        <div className="main">
+
+          <h3 className='problems'>{task}</h3>
+
+          <div>{showTips}</div>
+
+          <button onClick={() => { 
+            if(hello === 'Today we are making React Sandwich'){
+              setTask('You can now replace the button with Button component')
             }
-          }
-        >Let's Do It!</button>
+            setHello("Today we are making React Sandwich")
+            setShowTips("");
+            }}>Let's Do It!</button>
+          <button onClick={() => { setShowTips('When you click, above line shall change to --> Today we are making React Sandwich')}} >Show Tips</button>
+
+        </div>
       </header>
     </div>
   );
